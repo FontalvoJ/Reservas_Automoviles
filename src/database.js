005f34b7d6b85f0f6e7c802a3fa8a97285981e53
José David F <90539362+FontalvoJ@users.noreply.github.com>
2024-09-07 21:42:49 -0500

    update url database

--- src/database.js
+++ src/database.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
-//mongoosev.connect("mongodb+srv://fontalvomejiajosedavid54:CuVMgbpBmrBux2tW@api-node-aerofuel.uhm8jda.mongodb.net/?retryWrites=true&w=majority&appName=api-node-aerofuel")
+mongoosev.connect("mongodb+srv://fontalvomejiajosedavid54:CuVMgbpBmrBux2tW@api-node-aerofuel.uhm8jda.mongodb.net/?retryWrites=true&w=majority&appName=api-node-aerofuel")
 
-  mongoose
-    .connect("mongodb://0.0.0.0:27017/api-registro_vales")
+  //mongoose.connect("mongodb://0.0.0.0:27017/api-registro_vales")
     
     .then(() => console.log("🌟 Database is Connected"))
     .catch((err) => console.error("❌ Error connecting to the database:", err));
