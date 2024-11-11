68369db8dae3b6b6248851b9cf5ac7822371fed4
José David F <90539362+FontalvoJ@users.noreply.github.com>
2024-11-11 00:53:04 -0500

    New interface Sign In

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './pages/auth/sign-in/sign-in.component';
+import { HomeComponent } from './pages/home/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'signin', component: SignInComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
